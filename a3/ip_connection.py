55437a625a700f9fe04fc592f4cd78ee418155a9
AJ Po-Deziel <aj.podeziel@gmail.com>
2018-03-15 20:58:52 -0700

    Added initial packet parsing, class definition

--- a3/ip_connection.py
+++ a3/ip_connection.py
@@ -7,8 +7,10 @@ Created on: 2018-03-12
 Data structure to store IP connection data.
 """
 
+import statistics
+
 class IPConnection():
-    def __init__(self, src_ip, dest_ip, protocol):
+    def __init__(self, src_ip, dest_ip, timestamp):
         self.src_ip = src_ip
         self.dest_ip = dest_ip
-        self.protocol = protocol
+        self.timestamp = timestamp
--- a3/ip_trace_analysis.py
+++ a3/ip_trace_analysis.py
@@ -7,7 +7,10 @@ Created on: 2018-03-12
 Run IP Trace Analysis from IP datagrams gathered via traceroute.
 """
 
+import ip_connection
+import datetime
 import dpkt
+import statistics
 import sys
 
 
@@ -17,24 +20,49 @@ def main():
 
     # Read traceroute file
     capture_file = open(sys.argv[1], 'rb')
-    eth = dpkt.pcap.Reader(capture_file)
+    packet_capture = dpkt.pcap.Reader(capture_file)
+
+    ip_packets = []
 
     # Analyze traceroute file
+    for timestamp, raw_packet in packet_capture:
+        eth = dpkt.ethernet.Ethernet(raw_packet)
+        ip = eth.data
+
+        # Common packet info
+        ip_src = ip.src
+        ip_dest = ip.dest
+        packet_timestamp = datetime.datetime.utcfromtimestamp(timestamp)
+
+        # If packet is ICMP, handle as Windows packet
+        if isinstance(ip.data, dpkt.icmp.ICMP):
+            if ip.ttl >= 1:
+                icmp = ip.data
+                ip_packets.append()
+        # If packet is UDP, handle as Linux packet
+        elif isinstance(ip.data, dpkt.udp.UDP):
+            udp = ip.data
+            port_src = udp.sport
+            port_dest = udp.dport
+        else:
+            continue
+
+
 
     # TODO: IP Protocol Analysis - Output
-    print("The IP address of the source node: ")
-    print("The IP address of ultimate destination node: ")
-    print("The IP addresses of the intermediate destination nodes: ")
-    print("Router #")
-
-    print("The values in the protocol field of IP headers: ")
-    print("id: ")
-    print("id: ")
-
-    print("\n")
-
-    print("The number of fragments created from the original datagram is: ")
-    print("The avg RRT between (src IP) and (dest IP) is: (time), the s.d. is: (time)")
+    # print("The IP address of the source node: " + source_ip)
+    # print("The IP address of ultimate destination node: " + dest_ip )
+    # print("The IP addresses of the intermediate destination nodes: ")
+    # print("Router #")
+    #
+    # print("The values in the protocol field of IP headers: ")
+    # print("id: ")
+    # print("id: ")
+    #
+    # print("\n")
+    #
+    # print("The number of fragments created from the original datagram is: ")
+    # print("The avg RRT between {0} and {1} is: {2}, the s.d. is: {3}".format(source_ip, dest_ip, avg_rtt, sd_rtt))
 
     print("\n")
 
